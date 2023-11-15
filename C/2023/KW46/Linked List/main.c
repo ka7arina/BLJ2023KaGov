7aa0778a4e270ef394151b933679ada6f0368373
ka7arina <katarina.govedarica@edu.tbz.ch>
2023-11-15 09:35:38 +0100

    store adress

--- C/2023/KW46/Linked List/main.c
+++ C/2023/KW46/Linked List/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node
 {
-    int number;
+    int data;
     struct node *next;
 }
 node;
+
+int main() {
 
 // allocating the space of the node //
 struct node *a, *b;
     a = malloc(sizeof(struct node));
     b = malloc(sizeof(struct node));
+
+// set the values with a pointer //
+	a->data = 10;
+    b->data = 20;
+
+
+	a->next = b;
+    b->next = NULL;
+}
