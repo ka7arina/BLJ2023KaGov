bba75eb4e661b4c7371d4c16a92dc47f122b568c
ka7arina <katarina.govedarica@edu.tbz.ch>
2023-11-15 10:27:12 +0100

    linked list mit nodes erstellt

--- C/2023/KW46/Linked List/main.c
+++ C/2023/KW46/Linked List/main.c
@@ -11,15 +11,29 @@ node;
 int main() {
 
 // allocating the space of the node 
-struct node *a, *b;
+struct node *a, *b, *c, *d, *e, *f;
     a = malloc(sizeof(struct node));
     b = malloc(sizeof(struct node));
+	c = malloc(sizeof(struct node));
+	d = malloc(sizeof(struct node));
+	e = malloc(sizeof(struct node));
+	f = malloc(sizeof(struct node));
 
 // set the values with a pointer
 	a->data = 10;
     b->data = 20;
+	c->data = 30;
+	d->data = 40;
+	e->data = 50;
+	f->data = 60;
 
 // store the adress of b in the next variable of a
 	a->next = b;
-    b->next = NULL;
+	b->next = c;
+	c->next = d;
+	d->next = e;
+	e->next = f;
+    f->next = NULL;
+
+	printf("%d\n", f->data);
 }
