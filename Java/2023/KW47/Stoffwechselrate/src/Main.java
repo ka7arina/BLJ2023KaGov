20f01da536367c20ec0ea628971b1d2372867e8f
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2023-11-23 12:13:03 +0100

    finished stoffwechselrate

---
+++ Java/2023/KW47/Stoffwechselrate/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your weight in kg");
+        int weight = scanner.nextInt();
+        System.out.println("Please enter your height in cm");
+        int height = scanner.nextInt();
+        System.out.println("Please enter your age in years");
+        int age = scanner.nextInt();
+
+        double stoffwechselrate;
+
+        System.out.println("Are you male or female?\n" +
+                "Press 1 for Male\n" +
+                "Press 2 for female");
+        int gender = scanner.nextInt();
+        switch(gender) {
+            case 1:
+                stoffwechselrate = 66.47 + 13.7 * weight + 5 * height - 6.8 * age;
+                break;
+            case 2:
+                stoffwechselrate = 655.1 + 9.6 * weight + 1.8 * height - 4.7 * age;
+                break;
+            default:
+                System.out.println("Invalid input. Please enter 1 or 2.");
+                return;
+
+        }
+        System.out.println("The stoffwechselrate is: " + stoffwechselrate);
+    }
+}
