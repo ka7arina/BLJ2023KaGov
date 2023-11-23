d2d1c251924186ad0980cb3883c73ecf89618a24
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2023-11-23 11:13:40 +0100

    finished the basic christmas tree

--- Java/2023/KW47/Weihnachtsbaum/src/ch/noseryoung/blj/Main.java
+++ Java/2023/KW47/Weihnachtsbaum/src/ch/noseryoung/blj/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         int numRows = 7;
 
+        // repeats for the amount of rows there are
         for (int i = 1; i <= numRows; i++) {
-            // Print spaces before the stars
+            // prints spaces before the stars
             for (int j = 0; j < numRows - i; j++) {
                 System.out.print(" ");
             }
 
-            // Print the stars
+            // prints the stars
             for (int k = 0; k < 2 * i - 1; k++) {
                 System.out.print("*");
             }
