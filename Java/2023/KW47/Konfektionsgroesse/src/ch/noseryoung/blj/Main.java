c16809fc0718081aded7ee4050a1c7774a689064
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2023-11-24 08:58:56 +0100

    started the basic calculator

--- Java/2023/KW47/Konfektionsgroesse/src/ch/noseryoung/blj/Main.java
+++ Java/2023/KW47/Konfektionsgroesse/src/ch/noseryoung/blj/Main.java
@@ -9,19 +9,35 @@ Formel: Brustumfang / 2 -> für Frauen gilt: Brustumfang / 2 - 6 */
 entweder für F oder M -> Output die Konfektionsgrösse */
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to the Garment Size Calculator!!\n");
-        System.out.println("Are you female or male? (M / F)");
 
-        Scanner scanner = new Scanner(System.in);
-        String gender = scanner.next();
+        computeGarmentSize();
 
-        System.out.println("Please Input your chest measurement (in cm): ");
-
+        System.out.println("This is your Garment Size: " );
 
     }
 
     public static void computeGarmentSize() {
 
+        int GarmentSize;
+
+        System.out.println("Are you female or male? (M = 1 / F = 2)");
+        Scanner scanner = new Scanner(System.in);
+        int gender = scanner.nextInt();
+
+        System.out.println("Please Input your chest measurement (in cm): ");
+        int chestMeasurement = scanner.nextInt();
+
+        switch (gender) {
+            case 1: GarmentSize = (int)chestMeasurement / 2;
+                break;
+            case 2: GarmentSize = (int)chestMeasurement / 2 - 6;
+                break;
+            default: System.out.println("Please Input a Valid number!!!!!!!");
+                break;
+        }
     }
 }
