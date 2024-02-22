9313aa6994b3671dd4a68eddaa479858d7405e80
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-02-22 08:07:59 +0100

    added ANSI colour codes, function to switch the input and the random word to lowercase (so it doesnt see an upper case letter as a wrong letter)

--- Java/2024/KW08/Wordle/textanalyzer.java
+++ Java/2024/KW08/Wordle/textanalyzer.java
@@ -7,11 +7,18 @@ import java.util.Random;
 public class textanalyzer {
 
     private static final int MAX_ATTEMPTS = 6;
+    private static final String GREEN = "\u001B[0;32m";
+    private static final String YELLOW = "\u001B[0;33m";
+    private static final String RESET = "\u001B[0;0m";
+
 
     public static void wordleMechanic() {
         String fileName = "C:/Projects/BLJ2023KaGov/Java/2024/KW08/Wordle/wordlist.txt";
         ArrayList<String> wordList = readWordList(fileName); // Populate wordList
         String randomWord = getRandomWord(wordList);
+
+        //
+        randomWord = randomWord.toLowerCase();
 
         if (wordList.isEmpty()) {
             System.out.println("Word list is empty or file not found.");
@@ -26,19 +33,31 @@ public class textanalyzer {
         for (int attempts = 0; attempts < MAX_ATTEMPTS; attempts++) {
             if (attempts < MAX_ATTEMPTS - 1) {
                 System.out.println("     Input your guess:");
+                System.out.println("You have " + (MAX_ATTEMPTS-attempts) + " guess(es) left.");
                 String userinput = scanner.nextLine();
 
-                StringBuilder hint = new StringBuilder();
-                char guessedChar = userinput.charAt(attempts);
-                char secretChar = randomWord.charAt(attempts);
+                userinput = userinput.toLowerCase();
 
-                if (guessedChar == secretChar) {
-                    hint.append("●"); // correct letter correct position
-                } else if (randomWord.contains(String.valueOf(guessedChar))) {
-                    hint.append("○"); // correct letter, wrong position
-                } else {
-                    hint.append("◌"); // incorrect letter
+                StringBuilder hint = new StringBuilder();
+
+                for (int i = 0; i < userinput.length(); i++) {
+
+
+                    char guessedChar = userinput.charAt(i);
+                    char secretChar = randomWord.charAt(i);
+
+                    if (userinput.equals(randomWord)) {
+                        System.out.println("Congrats! You won!");
+                        i = 0;
+                    } else if (guessedChar == secretChar) {
+                        hint.append(GREEN + guessedChar + RESET); // correct letter correct position
+                    } else if (randomWord.contains(String.valueOf(guessedChar))) {
+                        hint.append(YELLOW + guessedChar + RESET); // correct letter, wrong position
+                    } else {
+                        hint.append(guessedChar); // incorrect letter
+                    }
                 }
+                System.out.println(hint.toString());
             } else {
                 System.out.println("You are out of guesses! The word was " + randomWord);
             }
