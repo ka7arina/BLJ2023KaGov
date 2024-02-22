67d39612ebb5ff7a81016a2fc49e3efc5f202b2a
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-02-22 11:37:39 +0100

    added filereader.java , added all the file reading and word functions into it

--- Java/2024/KW08/Wordle/filereader.java
+++ Java/2024/KW08/Wordle/filereader.java
@@ -1,6 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
+public class filereader {
+
+    public static ArrayList<String> readWordList(String fileName) {
+        ArrayList<String> wordList = new ArrayList<>();
+        File file = new File(fileName);
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String word = scanner.nextLine().trim();
+                if (!word.isEmpty()) {
+                    wordList.add(word);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+
+        return wordList;
+    }
+
+    public static String getRandomWord(ArrayList<String> wordList) {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(wordList.size());
+        return wordList.get(randomIndex);
+    }
+
+}
 
