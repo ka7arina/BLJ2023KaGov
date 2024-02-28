6979604e46b629c1947976ef6f6bd294e414c95a
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-02-28 11:39:40 +0100

    nevermind the other commit, changed file location and drag-and-dropped it into this one after resolving some technical issues

--- Java/2024/KW09/Java Exceptions/Exceptions/Starter.java
+++
@@ -1,6 +0,0 @@
-public class Starter {
-    public static void main(String[] args) {
-        TriangleApp app = new TriangleApp("Noser Young", "Katarina Govedarica");
-        // TODO: Start triangle app
-    }
-}
