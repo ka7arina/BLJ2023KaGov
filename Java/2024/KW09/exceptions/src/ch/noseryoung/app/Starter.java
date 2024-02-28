df576e2ca3686a7109a32352fd1c691c624fcdd0
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-02-28 14:28:57 +0100

    (for some reason it shows these in the unversioned files) started the exceptions challenge, added the userinput (doesnt work yet), get current year, printed header

---
+++ Java/2024/KW09/exceptions/src/ch/noseryoung/app/Starter.java
@@ -0,0 +1,8 @@
+package ch.noseryoung.app;
+
+public class Starter {
+  public static void main(String[] args) {
+    TriangleApp app = new TriangleApp("Noser Young", "Katarina Govedarica");
+	// TODO: Start triangle app
+  }
+}
