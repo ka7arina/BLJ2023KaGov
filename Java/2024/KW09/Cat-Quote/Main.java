f6e9d7003380a3a402c11d51711730acf47b2b2e
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-01 16:01:02 +0100

    added the main and the getQuote function

--- Java/2024/KW09/Cat-Quote/Main.java
+++ Java/2024/KW09/Cat-Quote/Main.java
@@ -1,3 +1,10 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class Main {
+    public static void main(String[] args) {
+        QuoteGetter.getQuote();
+    }
 }
--- Java/2024/KW09/Cat-Quote/QuoteGetter.java
+++ Java/2024/KW09/Cat-Quote/QuoteGetter.java
@@ -1,3 +1,42 @@
-public class QuoteGetter {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class QuoteGetter {
+    public static void getQuote() {
+        try {
+            // the url that you want to get content from
+            URL url = new URL("https://zenquotes.io/api/random/");
+
+            // opening the connection with the server
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // get method ???
+            connection.setRequestMethod("GET");
+
+            // response code (???)
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            // read the url content if the connection is successful
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // print out answer
+                System.out.println("Response Content: " + response.toString());
+            } else {
+                System.out.println("GET Request failed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
