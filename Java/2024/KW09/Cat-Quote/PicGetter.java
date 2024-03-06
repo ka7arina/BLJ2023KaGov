3d0fb7a998c1e79242d1a015883a709d1f0578a2
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-06 08:39:19 +0100

    started teilaufgabe 2, added the buffered reader

--- Java/2024/KW09/Cat-Quote/PicGetter.java
+++ Java/2024/KW09/Cat-Quote/PicGetter.java
@@ -1,3 +1,32 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class PicGetter {
 
+    public static void getCatPic() {
+        try {
+            URL url = new URL("https://cataas.com/cat");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
