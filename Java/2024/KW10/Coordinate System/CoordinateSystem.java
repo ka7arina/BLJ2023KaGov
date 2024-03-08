7648aad0ce3117ca634b02aa31a741462d1e30ed
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-08 13:24:45 +0100

    added an exception

--- Java/2024/KW10/Coordinate System/CoordinateSystem.java
+++ Java/2024/KW10/Coordinate System/CoordinateSystem.java
@@ -3,6 +3,16 @@ import java.util.List;
 
 public class CoordinateSystem {
 
+    private int coordinateSystemSize;
+    private List<CSPoint> points;
+
+    public CoordinateSystem(int coordinateSystemSize) throws IllegalAccessException {
+        if(coordinateSystemSize <= 0 || coordinateSystemSize % 20 != 0){
+            throw new IllegalAccessException();
+        }
+        this.coordinateSystemSize = coordinateSystemSize;
+        this.points = new ArrayList<>();
+    }
 
     public CSPoint[] getAllPoints(){
         return points.toArray(new CSPoint[0]);
