4c5d8ef2d4068a8b318512b1a6f436ae9702bd43
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-08 14:07:03 +0100

    added the main method

--- Java/2024/KW10/Coordinate System/Starter.java
+++ Java/2024/KW10/Coordinate System/Starter.java
@@ -1,3 +1,7 @@
 public class Starter {
-
+    public static void main(String[] args) throws IllegalAccessException {
+        CoordinateSystem cs = new CoordinateSystem(500);
+        cs.addPoints(new CSPoint(-30, 60));
+        new CSRenderer(cs, 1, 3);
+    }
 }
