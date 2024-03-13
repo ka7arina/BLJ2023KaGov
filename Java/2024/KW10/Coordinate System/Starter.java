3d11d346cf95870921023021c629005f8eda2818
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-13 16:14:32 +0100

    played around with some line designs then changed it back to normal

--- Java/2024/KW10/Coordinate System/Starter.java
+++ Java/2024/KW10/Coordinate System/Starter.java
@@ -2,13 +2,12 @@ public class Starter {
     public static void main(String[] args) throws IllegalAccessException {
         CoordinateSystem cs = new CoordinateSystem(500);
 
-        // creates points (coordinates)
-        cs.addLineSegment(new CSLineSegment(new CSPoint(-200, 60),new CSPoint(-30, 60)));
-        cs.addLineSegment(new CSLineSegment(new CSPoint(-30, 60),new CSPoint(-30, 60)));
+        // creates points (coordinates) the first CSPoint defines from where the line goes
+        cs.addLineSegment(new CSLineSegment(new CSPoint(250, -250),new CSPoint(-250, 250)));
+        cs.addLineSegment(new CSLineSegment (new CSPoint(68, -80),new CSPoint (-80, 68)));
 
-
+        // renders everything
         new CSRenderer(cs, 1, 3);
-
 
     }
 }
