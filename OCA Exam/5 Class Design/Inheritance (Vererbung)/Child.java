ce3afbcf567237e458f73b650562df14adae43bb
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-14 10:29:22 +0100

    tested the 'super' keyword and added comments

--- OCA Exam/5 Class Design/Inheritance (Vererbung)/Child.java
+++ OCA Exam/5 Class Design/Inheritance (Vererbung)/Child.java
@@ -5,6 +5,12 @@ public class Child extends Parent {
     // overrides the meow function in parent
     @Override
     public void meow() {
+
+        /* the super keyword accesses the superclass (which is parent) and then prints out both
+        * that and this print statement
+        */
+
+        super.meow();
         System.out.println("I am " +getAge()+ " years old and say MEOW!");
     }
 }
