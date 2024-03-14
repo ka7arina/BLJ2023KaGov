72ac4922409786bea1780a50214ec95c07207910
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-14 09:18:00 +0100

    created the classes and filled out parent class with a variable, a getter and a setter

---
+++ OCA Exam/5 Class Design/Inheritance (Vererbung)/Parent.java
@@ -0,0 +1,16 @@
+// class
+public class Parent {
+
+    // variable
+    public int age;
+
+    // getter
+    public int getAge() {
+        return age;
+    }
+
+    // setter
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
