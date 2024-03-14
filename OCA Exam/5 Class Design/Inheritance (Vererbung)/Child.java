4ee45f65179dcf1917e4f58ecd532331a5b2692b
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-14 09:37:01 +0100

    added a comment

--- OCA Exam/5 Class Design/Inheritance (Vererbung)/Child.java
+++ OCA Exam/5 Class Design/Inheritance (Vererbung)/Child.java
@@ -1,5 +1,7 @@
 // class that extends the parent (child class)
 public class Child extends Parent {
+
+    // meow method and print function
     public void meow() {
         System.out.println("The " +getAge() + " year old cat says MEOW!");
     }
