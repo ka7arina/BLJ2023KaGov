525372f256b31dea5f4125d19726c0ce230105cc
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-14 09:27:06 +0100

    created the child class

--- OCA Exam/5 Class Design/Inheritance (Vererbung)/Child.java
+++ OCA Exam/5 Class Design/Inheritance (Vererbung)/Child.java
@@ -1,2 +1,6 @@
-public class Child {
+// class that extends the parent (child class)
+public class Child extends Parent {
+    public void meow() {
+        System.out.println("The " +getAge() + " year old cat says MEOW!");
+    }
 }
