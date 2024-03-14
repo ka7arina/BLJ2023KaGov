c57cd01579b052f64468e41332e59f3a52b39ff1
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-14 11:16:17 +0100

    adjusted comments

--- OCA Exam/5 Class Design/Inheritance (Vererbung)/Child.java
+++ OCA Exam/5 Class Design/Inheritance (Vererbung)/Child.java
@@ -2,15 +2,15 @@
 public class Child extends Parent {
 
 
-    /* overrides the meow function in parent
-    * when you click on the little target circle it brings you to the superclass
-     */
+    /* overrides the meow function in 'Parent.java'.
+    * BTW: when you click on the little target circle it brings you to the superclass!
+    */
 
     @Override
     public void meow() {
 
-        /* the super keyword accesses the superclass (which is parent) and then prints out both
-        * that and this print statement
+        /* the super keyword accesses the superclass (which is 'Parent.java') and then prints out both
+        * that and the print statement below.
         */
 
         super.meow();
