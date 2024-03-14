93bc1ab0d5ef1dfd63f1bfcce4519bf05c8e6c15
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-14 09:33:27 +0100

    created the main method and ran the program (successfully)

--- OCA Exam/5 Class Design/Inheritance (Vererbung)/Main.java
+++ OCA Exam/5 Class Design/Inheritance (Vererbung)/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        // new instance of child
+        Child childClass = new Child();
+
+        // set the age of the child class
+        childClass.setAge(3);
+
+        // run the child class
+        childClass.meow();
     }
 }
