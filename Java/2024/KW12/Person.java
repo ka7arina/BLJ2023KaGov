ea7f48d7eba452ac76370831f6ecf08fcc7867b0
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-20 10:32:23 +0100

    created the variables and generated the getters and setters for Person.java

--- Java/2024/KW12/Person.java
+++ Java/2024/KW12/Person.java
@@ -1,7 +1,59 @@
+import java.time.LocalDate;
+
 public class Person {
+
+    // Variables
+    private String firstName;
+    private String lastName;
+    private LocalDate birthYear;
+    private String address;
+    private String phoneNumber;
 
     // Constructor
     public Person() {
 
     }
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(LocalDate birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
 }
