08023a1315647186df3a844788c761b3b54dae28
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-20 11:48:44 +0100

    changed the variable access from private to public

--- Java/2024/KW12/logic/Vehicle.java
+++ Java/2024/KW12/logic/Vehicle.java
@@ -4,10 +4,10 @@ import java.time.LocalDate;
 
 public class Vehicle {
 
-    private LocalDate year;
-    private String brand;
-    private String mileage;
-    private String color;
+    public LocalDate year;
+    public String brand;
+    public String mileage;
+    public String color;
 
     public Vehicle(LocalDate year, String brand, String mileage, String color) {
 
