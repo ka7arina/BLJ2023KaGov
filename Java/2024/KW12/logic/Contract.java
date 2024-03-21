dfefc6b2100fc684c0f2ed882917790695cc5795
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-21 13:41:54 +0100

    added variables to the contract class

--- Java/2024/KW12/logic/Contract.java
+++ Java/2024/KW12/logic/Contract.java
@@ -1,4 +1,15 @@
 package logic;
 
+import java.time.LocalDate;
+
 public class Contract {
+    public LocalDate startDate;
+    public LocalDate endDate;
+    public String contractModel;
+
+    public int leasingRate;
+
+    public void startContract() {
+
+    }
 }
