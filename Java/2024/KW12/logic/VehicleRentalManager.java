be43160693b7e4c4cb6ec88d0233058f39461ae9
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-21 08:46:25 +0100

    added a second customer to the arraylist

--- Java/2024/KW12/logic/VehicleRentalManager.java
+++ Java/2024/KW12/logic/VehicleRentalManager.java
@@ -8,15 +8,15 @@ public class VehicleRentalManager {
 
         // Create birthyear and customers
         LocalDate birth1 = LocalDate.of(2000, 11, 27);
-        birth1.getYear();
-        Person person1 = new Person("John", "Miller", birth1, "Heckengasse 2, 7035 Oerlikon", "+41 65 765 283");
+        Person customer1 = new Person("John", "Miller", birth1, "Heckengasse 2, 7035 Oerlikon", "+41 65 765 283");
 
-        LocalDate birth2 = LocalDate.of(2018, 8, 27);
-        birth2.getYear();
+        LocalDate birth2 = LocalDate.of(2018, 8, 14);
+        Person customer2 = new Person("Maria", "Smith", birth2, "Rosengartenstrasse 8, 8037 Zürich", "+41 76 897 65 23");
 
         // Create Arraylist for customers
-        ArrayList<Person> customers = new ArrayList<Person>();
-        customers.add(person1);
+        ArrayList<Person> customers = new ArrayList<>();
+        customers.add(customer1);
+        customers.add(customer2);
 
         System.out.println(customers);
     }
