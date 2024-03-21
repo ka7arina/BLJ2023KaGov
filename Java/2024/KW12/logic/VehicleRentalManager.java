f4578513eca8f23b32e60130b771a052f8e5ccfb
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-21 08:36:51 +0100

    created arraylist and added first person to it for a test run

--- Java/2024/KW12/logic/VehicleRentalManager.java
+++ Java/2024/KW12/logic/VehicleRentalManager.java
@@ -1,19 +1,24 @@
 package logic;
-
-import vehicles.Compact;
-
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class VehicleRentalManager {
 
     public void customerList() {
+
         // Create birthyear and customers
         LocalDate birth1 = LocalDate.of(2000, 11, 27);
         birth1.getYear();
         Person person1 = new Person("John", "Miller", birth1, "Heckengasse 2, 7035 Oerlikon", "+41 65 765 283");
-        System.out.println(person1.toString());
+
         LocalDate birth2 = LocalDate.of(2018, 8, 27);
         birth2.getYear();
+
+        // Create Arraylist for customers
+        ArrayList<Person> customers = new ArrayList<Person>();
+        customers.add(person1);
+
+        System.out.println(customers);
     }
 
     public void denyList() {
