d59660b02b1a4aadb19d69d32e6972204ab82874
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-21 13:42:56 +0100

    added customers and a denylist member in Starter

--- Java/2024/KW12/logic/Starter.java
+++ Java/2024/KW12/logic/Starter.java
@@ -1,14 +1,22 @@
 package logic;
 import vehicles.Camper;
 
+import java.time.LocalDate;
+
 public class Starter {
     public static void main(String[] args) {
 
         VehicleRentalManager vehicleRentalManager = new VehicleRentalManager();
 
+        Person customer1 = new Person("John", "Miller", LocalDate.of(2000, 11, 9), "Heckengasse 2, 7035 Oerlikon", "+41 65 765 283");
+
+        Person customer2 = new Person("Maria", "Smith", LocalDate.of(2018, 6, 25), "Rosengartenstrasse 8, 8037 Zürich", "+41 76 897 65 23");
+
         Camper camper = new Camper("Hymermobil", 1957, "HYMER", "20 mpg", "White", "Yes", "Yes", 2);
         vehicleRentalManager.addVehicle(camper);
-        System.out.println(vehicleRentalManager.vehicles);
 
+        Person denied1 = new Person("Joe", "Biden", LocalDate.of(1980, 7, 11), "The White House", "+200 987652 26");
+
+        vehicleRentalManager.addPersonToDenyList(denied1);
     }
 }
