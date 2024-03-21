9d81a453a6c65bf473c1f9e6801f412c11be1552
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-21 15:55:18 +0100

    fixed the array list output

--- Java/2024/KW12/logic/VehicleRentalManager.java
+++ Java/2024/KW12/logic/VehicleRentalManager.java
@@ -4,35 +4,32 @@ import java.util.ArrayList;
 
 public class VehicleRentalManager {
 
+
+    // Array Lists
     public ArrayList<Person> customerList = new ArrayList<>();
     public ArrayList<Person> denyList = new ArrayList<>();
     public ArrayList<Vehicle> vehicles = new ArrayList<>();
 
-    public void customerList() {
+    public void addCustomerList(Person onCustomerList) {
 
-        // Create birthyear and customers
-        LocalDate birth1 = LocalDate.of(2000, 11, 27);
-        Person customer1 = new Person("John", "Miller", birth1, "Heckengasse 2, 7035 Oerlikon", "+41 65 765 283");
-
-        LocalDate birth2 = LocalDate.of(2018, 8, 14);
-        Person customer2 = new Person("Maria", "Smith", birth2, "Rosengartenstrasse 8, 8037 Zürich", "+41 76 897 65 23");
-
-        // Create Arraylist for customers
-        customerList.add(customer1);
-        customerList.add(customer2);
-
-        System.out.println(customerList);
+        // Create Arraylist for customer
+            customerList.add(onCustomerList);
+        System.out.println("Customer List" + customerList);
 
     }
 
-    public void addPersonToDenyList(Person person) {
-        denyList.add(person);
-        System.out.println(denyList);
+    public void addPersonToDenyList(Person denylisted) {
+        denyList.add(denylisted);
+        System.out.println("Deny List: " + denyList);
 
     }
 
     public void addVehicle(Vehicle vehicle) {
     vehicles.add(vehicle);
+        System.out.println("Vehicles" + vehicles);
     }
 
+    public void createContract() {
+
+    }
 }
