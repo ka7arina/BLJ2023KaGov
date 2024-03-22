551c93484c7feedaf4e1502a3059ab5f6d1b76ab
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-22 08:29:23 +0100

    added the Array List for the contracts

--- Java/2024/KW12/logic/VehicleRentalManager.java
+++ Java/2024/KW12/logic/VehicleRentalManager.java
@@ -4,32 +4,30 @@ import java.util.ArrayList;
 
 public class VehicleRentalManager {
 
-
     // Array Lists
     public ArrayList<Person> customerList = new ArrayList<>();
     public ArrayList<Person> denyList = new ArrayList<>();
     public ArrayList<Vehicle> vehicles = new ArrayList<>();
+    public ArrayList<Contract> contracts = new ArrayList<>();
 
     public void addCustomerList(Person onCustomerList) {
 
         // Create Arraylist for customer
             customerList.add(onCustomerList);
         System.out.println("Customer List" + customerList);
-
     }
 
     public void addPersonToDenyList(Person denylisted) {
         denyList.add(denylisted);
         System.out.println("Deny List: " + denyList);
-
     }
 
     public void addVehicle(Vehicle vehicle) {
     vehicles.add(vehicle);
-        System.out.println("Vehicles" + vehicles);
+        System.out.println("Vehicles: " + vehicles.toString());
     }
 
-    public void createContract() {
-
+    public void createContract(Contract newContract) {
+        contracts.add(newContract);
     }
 }
