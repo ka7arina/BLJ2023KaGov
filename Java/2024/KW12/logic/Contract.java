168bbbf79d3076a231364a8c53e0a7d18f84a5f6
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-22 15:17:54 +0100

    added getters and setters for contract

--- Java/2024/KW12/logic/Contract.java
+++ Java/2024/KW12/logic/Contract.java
@@ -2,13 +2,68 @@ package logic;
 import java.time.LocalDate;
 
 public class Contract {
+
+    // Variables
+    public String person;
     public LocalDate startDate;
     public LocalDate endDate;
     public String contractModel;
-
     public int leasingRate;
 
-    public void startContract() {
+    public String vehicle;
 
+    public Contract(String person, String vehicle, LocalDate startDate, LocalDate endDate, String contractModel, int leasingRate) {
+        this.person = person;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.contractModel = contractModel;
+        this.leasingRate = leasingRate;
+        this.vehicle = vehicle;
+
+    }
+
+
+
+    // Getters and Setters
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getContractModel() {
+        return contractModel;
+    }
+
+    public void setContractModel(String contractModel) {
+        this.contractModel = contractModel;
+    }
+
+    public int getLeasingRate() {
+        return leasingRate;
+    }
+
+    public void setLeasingRate(int leasingRate) {
+        this.leasingRate = leasingRate;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + person +
+                ", Vehicle: " + vehicle +
+                ", Starts: " + startDate +
+                ", Ends: " + endDate +
+                ", Leasing Rate: " + leasingRate +
+                ", Text: '" + contractModel;
     }
 }
