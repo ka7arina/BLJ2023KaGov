af84e934722a2e29e5ca6c91ef7cc0e3e809c4fa
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-03-27 15:18:19 +0100

    added the function to add a vehicle and wrapped everything into a do while loop

--- Java/2024/KW12/logic/Admin.java
+++ Java/2024/KW12/logic/Admin.java
@@ -1,5 +1,4 @@
 package logic;
-
 import java.util.Scanner;
 
 public class Admin {
@@ -12,25 +11,74 @@ public class Admin {
 
     public void adminLogic() {
 
+        boolean exit = false;
+
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Welcome back admin!\n" +
-                "What would you like to do?");
+        System.out.println("Welcome back admin!\n");
 
-        String choose = userInput.nextLine();
+        do {
 
-        if (choose.equals("help")) {
-            System.out.println("see contracts -> scc\n" +
-                    "see vehicles -> sv\n" +
-                    "see customers -> sc\n");
-        }
+            System.out.println("What would you like to do?");
+
+            String choose = userInput.nextLine();
+
+            if (choose.equals("help")) {
+                System.out.println("see contracts -> scc\n" +
+                        "see vehicles -> sv\n" +
+                        "see customers -> sc\n" +
+                        "see denylist -> sd\n" +
+                        "add vehicle -> add vehicle\n");
+            }
 
             if (choose.equals("scc")) {
                 System.out.println("Contracts: " + vehicleRentalManager.contracts);
             }
 
-        if (choose.equals("sv")) {
-            VehicleRentalManager vehicles = new VehicleRentalManager();
-            System.out.println(vehicles.vehicles);
-        }
+            if (choose.equals("sv")) {
+                System.out.println("Vehicles: " + vehicleRentalManager.vehicles);
+            }
+
+            if (choose.equals("sc")) {
+                System.out.println("Customers: " + vehicleRentalManager.customerList);
+            }
+
+            if (choose.equals("sd")) {
+                System.out.println("Deny List: " + vehicleRentalManager.denyList);
+            }
+
+            if (choose.equals("add vehicle")) {
+
+                Scanner input = new Scanner(System.in);
+
+                System.out.println("Name: ");
+                String name = input.nextLine();
+
+                System.out.println("Year: ");
+                int year = input.nextInt();
+
+                Scanner input2 = new Scanner(System.in);
+
+                System.out.println("Brand: ");
+                String brand = input2.nextLine();
+
+                System.out.println("Mileage: ");
+                int mileage = input2.nextInt();
+
+                Scanner input3 = new Scanner(System.in);
+
+                System.out.println("Color: ");
+                String color = input3.nextLine();
+
+                Vehicle newVehicle = new Vehicle(name, year, brand, mileage, color);
+                vehicleRentalManager.addVehicle(newVehicle);
+
+                System.out.println("Successfully added the vehicle!");
+            }
+
+            if (choose.equals("exit")) {
+                exit = true;
+                System.out.println("bye!");
+            }
+        } while (!exit);
     }
 }
