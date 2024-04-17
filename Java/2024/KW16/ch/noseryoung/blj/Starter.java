aa996664b403ccff953767339c33a599e319c427
ka7arina <142780600+KatarinaGovedarica@users.noreply.github.com>
2024-04-17 15:26:20 +0200

    added main

---
+++ Java/2024/KW16/ch/noseryoung/blj/Starter.java
@@ -0,0 +1,11 @@
+package ch.noseryoung.blj;
+
+import java.io.IOException;
+
+public class Starter {
+    public static void main(String[] args) throws IOException {
+
+        Panels panels = new Panels();
+        panels.writeImage();
+    }
+}
